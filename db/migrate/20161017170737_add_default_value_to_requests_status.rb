888342bbc099eb52d150e9887f7886260b97f5fe
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-10-18 14:02:29 -0400

    pushing to heroku

--- db/migrate/20161017170737_add_default_value_to_requests_status.rb
+++ db/migrate/20161017170737_add_default_value_to_requests_status.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueToRequestsStatus < ActiveRecord::Migration[5.0]
-  def change
-  	change_column :requests, :status, :integer, default: 0
-  end
+  # def change
+  # 	change_column :requests, :status, :integer, default: 0
+  # end
 end
