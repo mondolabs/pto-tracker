30d49ed1f2cbad2d48a0cb58714ba4a6bbfd1f19
Ross Wittenberg <ross.wittenberg@gmail.com>
2016-10-18 15:08:28 -0400

    pushing to heroku

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,8 @@
 class Team < ApplicationRecord
-	# has_many :users, dependent: :destroy
-	# belongs_to :department
+	has_many :users, dependent: :destroy
+	belongs_to :department
 
-	# def manager
-	# 	User.find manager_id
-	# end
+	def manager
+		User.find manager_id
+	end
 end
